a06654ed66527d99f53f018b8272561888f2e00d
Shane Emmons <shane@emmons.io>
2013-12-04 11:58:36 -0800

    show how Datamappify::Lazy breaks app

---
+++ bad.rb
@@ -0,0 +1,67 @@
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+Bundler.setup
+
+require "active_support/core_ext"
+
+Sequel.extension(:migration)
+DB = Sequel.sqlite
+
+Sequel.migration do
+  change do
+    create_table?(:people) do
+      primary_key :id, :type => "INTEGER"
+      column :name, "STRING"
+    end
+
+    create_table?(:books) do
+      primary_key :id, :type => "INTEGER"
+      column :name, "STRING"
+      column :person_id, "INTEGER"
+    end
+  end
+end.apply(DB, :up)
+
+Datamappify.config do |c|
+  c.default_provider = :Sequel
+end
+
+class Book
+  include Datamappify::Entity
+  include Datamappify::Lazy
+
+  attribute :name, String
+
+  belongs_to :person
+end
+
+class Person
+  include Datamappify::Entity
+  include Datamappify::Lazy
+
+  attribute :name, String
+
+  has_many :books, :via => Book
+end
+
+class BookRepository
+  include Datamappify::Repository
+
+  for_entity Book
+
+  map_attribute :name, :to => "Book#name"
+end
+
+class PersonRepository
+  include Datamappify::Repository
+
+  for_entity Person
+
+  map_attribute :name, :to => "Person#name"
+
+  references :books, :via => BookRepository
+end
+
+p = PersonRepository.save(Person.new(:name => "Bob"))
+PersonRepository.find(p.id)
